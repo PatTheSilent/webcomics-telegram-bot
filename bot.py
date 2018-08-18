e195ddeda1467bc18ce813dae9a0534cc04e33b5
Patryk Cichy <patryk.d.cichy@gmail.com>
2018-08-18 14:13:34 +0200

    Replace communication with the Telegram API with the python-telegram-bot

--- bot.py
+++ bot.py
@@ -1,71 +1,97 @@
+"""Simple Telegram bot for getting the latest web comics"""
+
 import json
 import logging
 import os
 import sys
-import logging
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(HERE, "./vendored"))
 
-import requests
 import feedparser
+import telegram
+from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters
+
+TOKEN = os.environ['TELEGRAM_TOKEN']
+FEED_MAP = {
+    "smbc": {
+        "url": "https://www.smbc-comics.com/comic/rss",
+    },
+    "xkcd": {
+        "url": "https://xkcd.com/rss.xml"
+    },
+    "ch": {
+        "url": "http://feeds.feedburner.com/Explosm"
+    },
+    "bugbash": {
+        "url": "http://feeds.feedburner.com/BugBash"
+    },
+    "dilbert": {
+        "url": "http://feed.dilbert.com/dilbert/daily_strip"
+    },
+    "oglaf": {
+        "url": "https://www.oglaf.com/feeds/rss/"
+    },
+    "myjetpack": {
+        "url": "http://myjetpack.tumblr.com/rss"
+    },
+    "frogpants": {
+        "url": "https://www.frogpants.com/2018?format=rss"
+    },
+    "geekandpoke": {
+        "url": "http://feeds.feedburner.com/GeekAndPoke"
+    },
+    "oatmeal": {
+        "url": "http://feeds.feedburner.com/oatmealfeed"
+    }
+}
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-TOKEN = os.environ['TELEGRAM_TOKEN']
-BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
-FEED_MAP = {
-    "smbc": "https://www.smbc-comics.com/comic/rss"
-}
-
-class WebComicBot:
-    def __init__(self, chat_id):
-        self.chat_id = chat_id
-
-    @classmethod
-    def get_latest(cls, comic):
-        if comic in FEED_MAP:
-            feed = feedparser.parse(FEED_MAP[comic])
-            return feed['entries'][0]['title'] + "\n" + feed['entries'][0]['link']
-        logger.info("Comic %s not found in mapping", comic)    
-        return "Sorry {0} is not supported yet".format(comic)
-
-    def get_command(self, message):
-        logger.debug("Parsing user's /get command: %s", message)
-        command_content = message.replace("/get ", "")
-        return self.get_latest(command_content)
-
-    def create_response(self, message):
-        if message.startswith("/start"):
-            logger.debug("User sent a /start command")
-            return "It's a shame but this command doesn't do anything actually"
-        if message.startswith("/get"):
-            logger.debug("User sent a /get command")
-            return self.get_command(message)
-
-        return "I don't know what you mean. Please try again"
-
-    def send_response(self, message):
-        data = {"text": message.encode("utf8"), "chat_id": self.chat_id}
-        logger.debug("Sending message %s to chat %s", data["text"], data["chat_id"])
-        requests.post(BASE_URL + "/sendMessage", data)
-
-    def respond(self, message):
-        response = self.create_response(message)
-        self.send_response(response)
+def get_latest(comic):
+    logger.info("Searching for comic %s.", comic)
+    if comic in FEED_MAP:
+        feed = feedparser.parse(FEED_MAP[comic]["url"])
+        return (
+            feed['entries'][0]['title'] + "\n" +
+            feed['entries'][0]['link'])
+    logger.info("Comic %s not found in mapping", comic)
+    return "Sorry {0} is not supported yet".format(comic)
 
 
-def bot(event, context):
+def get(bot, update, args):
+    logger.info('Args: {}'.format(args))
+    if len(args):
+        if args[0] == "all":
+            for comic in FEED_MAP:
+                bot.send_message(
+                    chat_id=update.message.chat_id,
+                    text=get_latest(comic))
+        else:
+            bot.send_message(
+                chat_id=update.message.chat_id,
+                text=get_latest(args[0]))
+    else:
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text="I need something to work with")
+
+
+def webcomic_bot(event, context):
+    """Entrypoint for AWS Lambda"""
+    logger.info('Event: %s', event)
+    logger.info("Request ID: %s", context.aws_request_id)
+
     try:
+        bot = telegram.Bot(TOKEN)
+        dispatcher = Dispatcher(bot, None, workers=0)
         data = json.loads(event["body"])
-        message = str(data["message"]["text"])
-        chat_id = data["message"]["chat"]["id"]
-        logger.info("Received message %s from %s", message, chat_id)
-        bot = WebComicBot(chat_id)
-        bot.respond(message)
+        update = telegram.update.Update.de_json(data, bot)
+        dispatcher.add_handler(CommandHandler('get', get, pass_args=True))
+        dispatcher.process_update(update)
 
-    except Exception as e:
-        print(e)
+    except Exception as error:
+        logger.exception(error)
 
     return {"statusCode": 200}
